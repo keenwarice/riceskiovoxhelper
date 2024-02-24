916a22066f037d682695bc62ba9ad67a2077da13
Rice <126218083+keenwarice@users.noreply.github.com>
2024-02-23 17:23:55 -0800

    Update script.js

--- new-tab/script.js
+++ new-tab/script.js
@@ -7,19 +7,15 @@
         function performSearch() {
             let inputValue = document.querySelector('.searchbar').value.trim();
 
-            // Check if the document is inside an iframe
-            if (window.self !== window.top) {
-                // If inside an iframe, open the specified URL and close the tab
-                window.open('https://keenwa.x10.mx/riceos/joaquinrules', '_blank').close();
-            } else {
-                // Check if the entered value looks like a URL
-                if (/^(https?|ftp):\/\/[^\s/$.?#].[^\s]*$/.test(inputValue)) {
+
+            // Check if the entered value looks like a URL
+            if (/^(https?|ftp):\/\/[^\s/$.?#].[^\s]*$/.test(inputValue)) {
                     window.location.href = inputValue + '#translate.google.com';
-                } else {
+            } else {
                     window.location.href = 'https://google.com/search?q=' + inputValue + "#translate.google.com";
-                }
             }
         }
+        
 
         function redirectToYouTube() {
             window.location.href = 'https://www.youtube.com/watch?v=dQw4w9WgXcQ';
